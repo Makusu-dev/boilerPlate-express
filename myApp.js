2a873c09c87bde90c679368ca5369d34f082c6c1
Makusu-dev <maxime.jeannin@hotmail.fr>
2025-07-23 09:02:32 +0200

    feat use of env variables

--- myApp.js
+++ myApp.js
@@ -1,5 +1,37 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
+
+app.use(function(req,res,next) {
+    console.log(req.method+' '+req.path+' - '+req.ip)
+    next()
+})
+
+let static = express.static(__dirname+'/public')
+app.use("/public",static)
+
+
+
+console.log('Hello World')
+
+app.get("/", (req,res) => {
+     
+    absolutePath = __dirname + '/views/index.html' 
+    console.log(absolutePath)  
+    res.sendFile(absolutePath)
+    });
+
+    app.get("/json", (req,res) => {
+        const style=process.env.MESSAGE_STYLE;
+        if(style=='uppercase') {
+            res.json({"message": "HELLO JSON"});
+        }
+        else {
+            res.json({"message": "Hello json"});
+        }        
+    })
+
 
 
 
